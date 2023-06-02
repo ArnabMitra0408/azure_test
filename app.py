9b6dd43d57f4f28c3f5eaa141e07cc120bc179eb
ArnabMitra0408 <arnabmitra490@gmail.com>
2023-06-02 15:21:10 +0530

    Added Application Insights

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import os
 import logging
+from applicationinsights.flask.ext import AppInsights
+
 
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=e01274f4-cb23-4bf0-b457-6b630481eef3;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/'))
+#logger = logging.getLogger(__name__)
+#logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=e01274f4-cb23-4bf0-b457-6b630481eef3;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/'))
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 app = Flask(__name__)
 
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = 'e01274f4-cb23-4bf0-b457-6b630481eef3'
+appinsights = AppInsights(app)
+
 @app.route('/')
 def index():
    print('Request for index page received')
-   logger.warning('Going to index Page')
+   #logger.warning('Going to index Page')
+
+   app.logger.warn('This is a warning log message')
+
    return render_template('index.html')
 
 @app.route('/favicon.ico')
@@ -27,10 +35,13 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
+       app.logger.debug('This is a debug log message')
+       app.logger.info('This is an information log message')
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
-       logger.error('Add Name Please')
+       #logger.error('Add Name Please')
+       app.logger.info('This is an information log message')
        return redirect(url_for('index'))
 
 
