a1078e11cffd21aa3546d06bb1d4f88f857b5e08
ArnabMitra0408 <arnabmitra490@gmail.com>
2023-06-05 08:50:41 +0530

    Logging changes made

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import logging
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
+logger_warning = logging.getLogger('warning')
+logger_error = logging.getLogger('error')
+logger_info = logging.getLogger('info')
+logger_critical = logging.getLogger('critical')
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   logger.warning("Its a Warning that index page is received")
+   logger_warning.warning("Its a Warning that index page is received",exc_info=True)
 
    return render_template('index.html')
 
@@ -24,16 +27,14 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       logger.error("It is a error log saying that Request for hello page is received")
+       logger_error.error("It is a error log saying that Request for hello page is received",exc_info=True)
     
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
-       logger.critical("It is a critical log saying no name is received")
+       logger_critical.critical("It is a critical log saying no name is received",exc_info=True)
        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
    app.run()
-   for num in range(5):
-        logger.warning(f"Log Entry - {num}")
